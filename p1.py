f19fdd4e9390fd27f66fa2d8f025104cad31a214
Alvaro Alonso <alvaroalonsosmr@gmail.com>
2019-06-11 22:33:28 +0200

    funcionesNivel1

--- p1.py
+++ p1.py
@@ -3,5 +3,13 @@ def sumatodos(limitTo):
     for i in range(0,limitTo):
         resultado+= 1
     return resultado
-print(sumatodos(100))
 
+def sumatodosloscuadrado(limitTo):
+    resultado=0
+    for i in range(limitTo+1):
+        resultado+= i*i
+        
+    return resultado
+
+print(sumatodos(100))
+print(sumatodosloscuadrado(3))
